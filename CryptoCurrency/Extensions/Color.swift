0d2033ebe7d2df744376082552a54902d21aa4a1
Yunus Olgun <y.olgun@gmail.com>
2025-03-29 20:45:21 +0300

    Setting up Xcode Project and a custom color theme

---
+++ CryptoCurrency/Extensions/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  CryptoCurrency
+//
+//  Created by yunus olgun on 29.03.2025.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
