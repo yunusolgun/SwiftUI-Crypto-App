f7161776d2cd5245561ce540531aa5d571e6a2d7
Yunus Olgun <y.olgun@gmail.com>
2025-03-29 20:30:41 +0300

    Initial Commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by yunus olgun on 29.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
