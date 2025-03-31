91493ba2895a532e8186c17c589aaf0334972509
Yunus Olgun <y.olgun@gmail.com>
2025-03-31 19:08:57 +0300

    Download coins using a View Model and Data Service class

--- CryptoCurrency/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoCurrency/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,17 +6,31 @@
 //
 
 import Foundation
+import Combine
 
 class HomeViewModel: ObservableObject {
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
+    private let dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
+    
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            self.allCoins.append(DeveloperPreview.instace.coin)
-            self.portfolioCoins.append(DeveloperPreview.instace.coin)
-        }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//            self.allCoins.append(DeveloperPreview.instace.coin)
+//            self.portfolioCoins.append(DeveloperPreview.instace.coin)
+//        }
+        
+        addSubscribers()
+    }
+    
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
     }
     
     
